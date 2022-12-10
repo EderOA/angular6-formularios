c6e0c86b86a5b7c3872c675d49b576f01a8dd8cd
Eder Ortiz <ederoa@hotmail.com>
2022-12-10 08:52:34 -0600

    main

---
+++ src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { emailPattern, nombreApellidoPattern, noPuedeSerederoa } from 'src/app/shared/validator/validaciones';
+import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html'
+})
+export class RegistroComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator] ],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerederoa] ],
+    password: ['', [Validators.required, Validators.minLength(6)] ],
+    password2: ['', [Validators.required] ],
+  },{
+    validators:[ this.validatorService.camposIguales('password', 'password2')]
+  })
+
+  get emailErrorMsg(): string{
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if ( errors?.required ){
+      return 'email es obligatorio';
+    }else if ( errors?.pattern ){
+      return 'El valor ingresado no tiene formato de correo';
+    }else if ( errors?.emailTomado){
+      return 'El email ya fue tomado';
+    }
+
+    return '';
+  }
+
+  constructor(private fb : FormBuilder,
+            private validatorService: ValidatorService,
+            private emailValidator: EmailValidatorService) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Eder Ortiz',
+      email: 'test1@test.com',
+      username: 'eder_ortiz',
+      password: '123456',
+      password2: '123456'
+    })
+  }
+
+  campoNoValido(campo: string){
+    return this.miFormulario.get(campo)?.invalid
+      && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario(){
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
+  }
+
+ 
+}
